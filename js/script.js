17880e693aa1ee110dbaf25bdd7ece6e0e7068f6
Sammy <sammyraesylv@gmail.com>
2023-03-25 18:37:57 -0400

    Idea 1
    Redesign,
    page links
    responsiveness
    beginner steps!

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const hoverEffect = document.getElementById('hover-effect');
-
-hoverEffect.addEventListener('mouseenter', function() {
-  hoverEffect.style.backgroundColor = '#0062cc';
-});
-
-hoverEffect.addEventListener('mouseleave', function() {
-  hoverEffect.style.backgroundColor = '#007bff';
-});
