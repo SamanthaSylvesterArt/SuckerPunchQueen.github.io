530281f69ecd371e1cbccfb6bbe967098d3435aa
vincture27 <91662922+vincture27@users.noreply.github.com>
2023-04-04 18:34:42 +0200

    fixed glitch effect
    and cleaned up code

--- js/glitcheffect.js
+++ js/glitcheffect.js
@@ -1,89 +1,70 @@
-const getSpeed = () => {
-    return ((parseInt(document.querySelector('input[name="speed-option"]:checked').value)) * 10) + 20;
-  };
-  
-  const animate = (e) => {
-    // return if already animating
-    if (e.target.getAttribute("data-filling") === "true") return;
-    e.target.setAttribute("data-filling", "true");
-    const fillmode = "both";
-  
-    const text = e.target.getAttribute("data-text");
-    const len = text.length;
-    const randomArr = Array.from({ length: len }, (_, i) =>
-      text[i] === " " ? "_" : ['X', '$', 'Y', '#', '?', '*', '0', '1', '+'][Math.floor(Math.random() * 9)]
-    );
-  
-    if (fillmode === "backwards") {
-      for (let i = len - 1; i >= 0; i--) {
-        if (i === 0) e.target.setAttribute("data-filling", "false");
-        setTimeout(() => {
-          randomArr.splice(i, 1, text[i]);
-          e.target.textContent = randomArr.join("");
-        }, (len - i) * getSpeed());
-      }
-    } else {
-      const isEven = len % 2 === 0;
-      for (let i = 0; i < len; i++) {
-        setTimeout(() => {
-          if (i === len - 1) e.target.setAttribute("data-filling", "false");
-          if (fillmode === "forwards" || (fillmode === "both" && i % 2 === 0)) {
-            randomArr.splice(i, 1, text[i]);
-          } else {
-            // runs only if fillmode is "both" and i is odd;
-            randomArr.splice(
-              isEven ? len - i : len - i - 1,
-              1,
-              isEven ? text[len - i] : text[len - i - 1]
-            );
-          }
-          e.target.textContent = randomArr.join("");
-        }, (i + 1) * getSpeed());
-      }
+const speed = 2;
+const animate = (e) => {
+  // return if already animating
+  if (e.target.getAttribute("data-filling") === "true") return;
+  e.target.setAttribute("data-filling", "true");
+  const fillmode = "both";
+
+  const text = e.target.getAttribute("data-text");
+  const len = text.length;
+  const randomArr = Array.from({ length: len }, (_, i) =>
+    text[i] === " " ? "_" : ['X', '$', 'Y', '#', '?', '*', '0', '1', '+'][Math.floor(Math.random() * 9)]
+  );
+
+
+    for (let i = len - 1; i >= 0; i--) {
+      if (i === 0) e.target.setAttribute("data-filling", "false");
+      setTimeout(() => {
+        randomArr.splice(i, 1, text[i]);
+        e.target.textContent = randomArr.join("");
+      }, (len - i) * speed);    
+  }
+};
+
+const setGlitch = (elements, onInit = true) => {
+  elements.forEach((el, idx) => {
+    el.setAttribute("data-text", el.textContent);
+    el.setAttribute("data-filling", "false");
+    el.getAttribute("data-event-type") === "click"
+      ? el.onclick = animate
+      : el.onmouseover = animate;
+
+    // animate on window load
+    if (onInit) {
+      setTimeout(() => {
+        animate({ target: el });
+      }, idx * speed);
     }
-  };
-  
-  const setGlitch = (elements, onInit = true) => {
-    elements.forEach((el, idx) => {
-      el.setAttribute("data-text", el.textContent);
-      el.setAttribute("data-filling", "false");
-      el.getAttribute("data-event-type") === "click"
-        ? el.onclick = animate
-        : el.onmouseover = animate;
-  
-      // animate on window load
-      if (onInit) {
-        setTimeout(() => {
-          animate({ target: el });
-        }, idx * 80);
-      }
+  });
+};
+
+const setLabelGlitch = (elements, label) => {
+  const handler = (e) => {
+    animate({
+      target: !label ? e.target.previousElementSibling : label
     });
   };
-  
-  const setLabelGlitch = (elements, label) => {
-    const handler = (e) => {
-      animate({
-        target: !label ? e.target.previousElementSibling : label
-      });
-    };
-    elements.forEach(el => el.oninput = handler);
-  };
-  
-  const init = () => {
-    setGlitch(document.querySelectorAll(".example"));
-    setGlitch(document.querySelectorAll("h2"));
-    setGlitch(document.querySelectorAll("label"));
-  
-    // glitch radio label on input change
-    // pass null to indicate that previousSibling should be used to target label
-    // or pass specific element
-    setLabelGlitch(
-      document.querySelectorAll('input[name="fill-option"]'),
-      null
-    );
-    setLabelGlitch(
-      document.querySelectorAll('input[name="speed-option"]'),
-      document.querySelector(".title-speed")
+  elements.forEach(el => el.oninput = handler);
+};
+
+const init = () => {
+  setGlitch(document.querySelectorAll("h1"));
+  setGlitch(document.querySelectorAll("h2"));
+  setGlitch(document.querySelectorAll("h3"));
+  setGlitch(document.querySelectorAll("h4"));
+  setGlitch(document.querySelectorAll("h5"));
+  setGlitch(document.querySelectorAll("h6"));
+
+  // glitch radio label on input change
+  // pass null to indicate that previousSibling should be used to target label
+  // or pass specific element
+  setLabelGlitch(
+    document.querySelectorAll('input[name="fill-option"]'),
+    null
+  );
+  setLabelGlitch(
+    document.querySelectorAll('input[name="speed-option"]'),
+    document.querySelector(".title-speed")
     );
   };
   
