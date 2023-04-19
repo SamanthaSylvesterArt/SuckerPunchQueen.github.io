539493b0e658424c5bf8dc84630ebcdf1e214aa4
Sammy <sammyraesylv@gmail.com>
2023-04-19 17:46:36 -0400

    cards to project page
    why it squish?

--- js/project.js
+++ js/project.js
@@ -17,3 +17,14 @@ if (typeof window.CSS.registerProperty === 'function') {
     document.body.classList.add('registerProperty-not-supported');
   }
   
+
+  document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
